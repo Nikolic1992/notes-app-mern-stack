a84bf5c42be50eacc11844929e6995e37baa67d2
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-06-30 18:10:13 +1000

    add cors for all routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,12 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
+// Import the database connection function
+import { connectDB } from "./config/db.js";
+
+// Import the routes for handling notes
+import notesRoutes from "./routes/notesRoutes.js";
+// Import the rate limiter middleware to control request rates
 import rateLimiter from "./middleware/rateLimiter.js";
 
 // Install dotenv to manage environment variables ( .env file )
@@ -13,6 +18,8 @@ const app = express();
 // Use the PORT environment variable or default to 5001 if not set
 const PORT = process.env.PORT || 5001;
 
+// Enable CORS for all routes
+app.use(cors());
 // Middleware to parse JSON request bodies
 app.use(express.json());
 // Middleware to limit the rate of incoming requests
