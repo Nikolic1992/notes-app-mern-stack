1f0fb483e45b336823a616eb0c112bfd8218e68e
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-07-01 19:38:49 +1000

    final commit, production and development mode setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 // Import the database connection function
 import { connectDB } from "./config/db.js";
 
@@ -18,8 +19,13 @@ const app = express();
 // Use the PORT environment variable or default to 5001 if not set
 const PORT = process.env.PORT || 5001;
 
-// Enable CORS for all routes
-app.use(cors());
+const __dirname = path.resolve();
+
+// Enable CORS for all routes if not in production mode
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "https://localhost:5173" }));
+}
+
 // Middleware to parse JSON request bodies
 app.use(express.json());
 // Middleware to limit the rate of incoming requests
@@ -29,6 +35,15 @@ app.use(rateLimiter);
 
 // Use the notes routes for all requests to /api/notes
 app.use("/api/notes", notesRoutes);
+
+// If in production mode, serve the React app which is located on index.html
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Connect to the database first before starting the server
 // This ensures that the server only starts if the database connection is successful
