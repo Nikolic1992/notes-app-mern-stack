45aea888e7b4e4e86681fb83f5d4e5345d94d3e7
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-07-01 18:22:17 +1000

    add delete functionality  to NoteCard component

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,12 +1,30 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
 
-function NoteCard({ note }) {
+function NoteCard({ note, setNotes }) {
+  async function handleDelete(e, id) {
+    // Prevent the default link behavior
+    e.preventDefault();
+
+    if (!window.confirm("Are you sure you want to delete this note?")) {
+      return;
+    }
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prevNotes) => prevNotes.filter((note) => note._id !== id));
+      toast.success("Note deleted successfully!");
+    } catch (error) {
+      console.error("Error deleting note:", error);
+      toast.error("Failed to delete note!");
+    }
+  }
   return (
     <Link
       to={`/note/${note._id}`}
-      className="card bg-base-100 hover:shadow-lg hover:shadow-gray-500/30 hover:scale-105 transition-all duration-300 border-t-4 border-solid border-[#00FF9D]"
+      className="card bg-base-100 hover:shadow-lg hover:shadow-gray-500/30 hover:scale-105 transition-all duration-300 border-t-4 border-solid border-[#5775fc]"
     >
       <div className="card-body">
         <h3 className="card-title text-base-content">{note.title}</h3>
@@ -17,7 +35,10 @@ function NoteCard({ note }) {
           </span>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
-            <button className="btn btn-ghost btn-xs text-error">
+            <button
+              className="btn btn-ghost btn-xs text-error"
+              onClick={(e) => handleDelete(e, note._id)}
+            >
               <Trash2Icon className="size-4" />
             </button>
           </div>
